ba2b42ca935d86a91888cfa46f5ec7120a94805d
fandanping <18602474229@163.com>
2018-07-11 17:02:34 +0800

    Initial commit

---
+++ src/main/java/com/neusoft/mpserver/MpServerApplication.java
@@ -0,0 +1,12 @@
+package com.neusoft.mpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpServerApplication.class, args);
+	}
+}
