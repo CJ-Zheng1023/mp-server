6aa6f5dc25f878070b1d56550f4363ba49a882ed
fandanping <18602474229@163.com>
2018-07-12 08:09:41 +0800

    base

--- src/main/java/com/neusoft/mpserver/domain/Constant.java
+++ src/main/java/com/neusoft/mpserver/domain/Constant.java
@@ -1,5 +1,14 @@
 package com.neusoft.mpserver.domain;
 
 public class Constant {
+    //注册成功code码值
     private static final String SUCCESS_REGISTER="1";
+    //注册失败code码值
+    private static final String FAIL_REGISTER="2";
+    //登录查询没有用户码值
+    private static final  String NOUSER_LOGIN="2";
+    //登录成功
+    private static final  String SUCCESS_LOGIN="1";
+    //登录查询密码错误
+    private static final  String FAILPASS_LOGIN="3";
 }
