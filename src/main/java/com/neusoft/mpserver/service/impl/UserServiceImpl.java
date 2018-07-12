b3d3697594b464dded5b6bf60db900f323a1a27e
fandanping <18602474229@163.com>
2018-07-12 16:33:14 +0800

    change

--- src/main/java/com/neusoft/mpserver/service/impl/UserServiceImpl.java
+++ src/main/java/com/neusoft/mpserver/service/impl/UserServiceImpl.java
@@ -9,7 +9,6 @@ import com.neusoft.mpserver.domain.User;
 import com.neusoft.mpserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
