75029867dfb6fe8121a2bfafee3e782eb06918d3
fandanping <18602474229@163.com>
2018-07-13 17:44:49 +0800

    commit

--- src/main/java/com/neusoft/mpserver/common/interceptor/OriginTnterceptor.java
+++ src/main/java/com/neusoft/mpserver/common/interceptor/OriginTnterceptor.java
@@ -2,7 +2,6 @@ package com.neusoft.mpserver.common.interceptor;
 import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 /**
