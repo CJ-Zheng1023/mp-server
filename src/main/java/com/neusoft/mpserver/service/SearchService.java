ca56c5be152938434735b3337a63eb6cd8e1c5c2
fandanping <18602474229@163.com>
2018-07-13 10:34:30 +0800

    查询文献列表接口完成

--- src/main/java/com/neusoft/mpserver/service/SearchService.java
+++ src/main/java/com/neusoft/mpserver/service/SearchService.java
@@ -1,14 +1,21 @@
 package com.neusoft.mpserver.service;
 
-import com.eprobiti.trs.TRSException;
 import com.neusoft.mpserver.common.domain.Pagination;
-import com.neusoft.mpserver.domain.Patent;
-
-import java.util.List;
 import java.util.Map;
 
+/**
+ * 根据ic查询案卷相关信息模块：service层接口
+ * @name fandp
+ * @email fandp@neusoft.com
+ */
 public interface SearchService {
     //查询案卷列表
     public Map<String,Object> searchPatentList(String ipc, Pagination pagination);
+
+    //查询中英文解释
+    public Map<String,Object> searchIpc(String ipc);
+
+    //查询案卷的标题、申请人、发明人、国省代码
+    public Map<String,Object> searchPatent(String an);
 
 }
