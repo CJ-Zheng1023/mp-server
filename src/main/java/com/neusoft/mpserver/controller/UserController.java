75d8fcd064692c478a6cd36cb5f9a25541f4b714
AfterWin <CJ_Zheng1023@hotmail.com>
2018-07-16 18:11:44 +0800

    解决cors

--- src/main/java/com/neusoft/mpserver/controller/UserController.java
+++ src/main/java/com/neusoft/mpserver/controller/UserController.java
@@ -3,12 +3,14 @@ package com.neusoft.mpserver.controller;
 import com.neusoft.mpserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- *   用户模块
+ * 用户模块
+ *
  * @name fandp
  * @email fandp@neusoft.com
  */
@@ -20,40 +22,42 @@ public class UserController {
 
     /**
      * 注册
+     *
      * @param username
      * @param password
      * @return
      */
     @PostMapping("/add")
-    public  String addUser(String username,String password){
-          return userService.addUser(username,password);
+    public String addUser(String username, String password) {
+        return userService.addUser(username, password);
     }
 
     /**
      * 登录
+     *
      * @param
      * @return
      */
     @PostMapping("/login")
-    public Map<String, Object> login(@RequestBody Map dataMap){
-        String username=(String )dataMap.get("username");
-        String password=(String)dataMap.get("password");
+    public Map<String, Object> login(@RequestBody Map postMap) {
+        String username = (String) postMap.get("username");
+        String password = (String) postMap.get("password");
         Map<String, Object> map = new HashMap<String, Object>();
-        map=userService.login(username,password);
+        map = userService.login(username, password);
         return map;
     }
 
     /**
      * 退出
+     *
      * @param
      * @return
      */
     @PostMapping("/logout")
-    public boolean logout(HttpServletRequest request){
-          String userId = (String) request.getAttribute("userId");
-          return userService.logout(userId);
+    public boolean logout(HttpServletRequest request) {
+        String userId = (String) request.getAttribute("userId");
+        return userService.logout(userId);
     }
-
 
 
 }
