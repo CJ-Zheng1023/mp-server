b5c7adb7dd6766d6de478bb45c8abecb1465f82e
fandanping <18602474229@163.com>
2018-07-16 11:01:31 +0800

    1.修改登录逻辑

--- src/main/java/com/neusoft/mpserver/dao/TokenRepository.java
+++ src/main/java/com/neusoft/mpserver/dao/TokenRepository.java
@@ -17,4 +17,7 @@ public interface TokenRepository   extends JpaRepository<Token,String> {
     @Query(value="delete from Token where userId=?1")
     public boolean  deleteByUserId(String userId);
 
+    //再次登录时，若token表存在此用户的token，删除。（删除前先查找）
+    public Token findByUserId(String userId);
+
 }
