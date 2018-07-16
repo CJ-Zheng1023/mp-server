23dd752679478700405c20e17c5ce7c22748663a
AfterWin <CJ_Zheng1023@hotmail.com>
2018-07-16 11:21:20 +0800

    完善检索业务逻辑

--- src/main/java/com/neusoft/mpserver/service/impl/SearchServiceImpl.java
+++ src/main/java/com/neusoft/mpserver/service/impl/SearchServiceImpl.java
@@ -10,10 +10,12 @@ import com.neusoft.mpserver.domain.Patent;
 import com.neusoft.mpserver.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 /**
  * ic业务查询模块：service层实现
  *
@@ -24,10 +26,11 @@ import java.util.Map;
 public class SearchServiceImpl implements SearchService {
     @Autowired
     private TrsEngine trsEngine;
+
     @Override
     public Map<String, Object> searchPatentList(String ipc, Pagination pagination) {
         Map<String, Object> map = new HashMap<String, Object>();
-        List<Map<String, String>> patentList = new ArrayList<Map<String, String>>();
+        List<Map<String, String>> patentList = null;
         Condition condition = new Condition();
         String searchIc = "IPC_MAIN='" + ipc + "'";
         condition.setExp(searchIc);
@@ -36,8 +39,12 @@ public class SearchServiceImpl implements SearchService {
         condition.setPagination(pagination);
         TrsResult tr = trsEngine.search(condition);
         List<Record> recordList = tr.getRecords();
-        for (int i = 0; i < recordList.size(); i++) {
-            patentList.add(AssembleData(recordList.get(i).getDataMap()));
+        int size = recordList.size();
+        if (size > 0) {
+            patentList = new ArrayList<Map<String, String>>();
+            for (int i = 0; i < size; i++) {
+                patentList.add(AssembleData(recordList.get(i).getDataMap()));
+            }
         }
         map.put("patentList", patentList);
         map.put("pagination", tr.getPagination());
@@ -46,22 +53,23 @@ public class SearchServiceImpl implements SearchService {
 
     /**
      * 默认取GK（公开）字段数据，若为空，则取SQ（授权）字段数据
-     * @param dataMap   检索出的trs行数据
-     * @return          组装后的数据
+     *
+     * @param dataMap 检索出的trs行数据
+     * @return 组装后的数据
      */
-    private Map<String, String> AssembleData(Map<String, String> dataMap){
+    private Map<String, String> AssembleData(Map<String, String> dataMap) {
         Map<String, String> resultMap = new HashMap<String, String>();
         String gkPn = dataMap.get(Constant.GK_PN);
         String prefix = Constant.GK_PREFIX;
-        if(gkPn == null || gkPn.equals("")){
+        if (gkPn == null || gkPn.equals("")) {
             prefix = Constant.SQ_PREFIX;
         }
         String[] mainFields = Constant.MAIN_FIELDS.split(",");
-        for(String f : mainFields){
+        for (String f : mainFields) {
             resultMap.put(f, dataMap.get(prefix + f));
         }
         String[] otherFields = Constant.OTHER_FIELDS.split(",");
-        for(String f : otherFields){
+        for (String f : otherFields) {
             resultMap.put(f, dataMap.get(f));
         }
         return resultMap;
@@ -69,21 +77,19 @@ public class SearchServiceImpl implements SearchService {
 
     @Override
     public Map<String, String> searchIpc(String ipc) {
-        Map<String,String> ipcResult =new HashMap<String, String>();
+        Map<String, String> ipcResult = null;
         Condition condition = new Condition();
         String searchIc = "IC='" + ipc + "'";
         condition.setExp(searchIc);
         condition.setDbName(Constant.IPC_DB);
         condition.setDisplayFields(Constant.IPC_FIELDS);
-        TrsResult tr=trsEngine.search(condition);
-        List<Record> recordList=tr.getRecords();
-        List<Map<String,String>>  ipcChEn=new ArrayList<Map<String, String>>();
-        if(recordList.size() == 0){
-            ipcResult.put("code", Constant.NO_IPC);
-            return ipcResult;
-        }
-        for (int i = 0; i < 1; i++) {
-            ipcResult = recordList.get(i).getDataMap();
+        TrsResult tr = trsEngine.search(condition);
+        List<Record> recordList = tr.getRecords();
+        if (recordList.size() > 0) {
+            ipcResult = new HashMap<String, String>();
+            for (int i = 0; i < 1; i++) {
+                ipcResult = recordList.get(i).getDataMap();
+            }
         }
         return ipcResult;
     }
