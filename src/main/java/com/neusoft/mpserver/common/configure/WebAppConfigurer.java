7d1c5ba224d4c40f8e6a97a69e8f8fea8dec6eec
AfterWin <CJ_Zheng1023@hotmail.com>
2018-07-16 17:58:33 +0800

    解决cors

--- src/main/java/com/neusoft/mpserver/common/configure/WebAppConfigurer.java
+++ src/main/java/com/neusoft/mpserver/common/configure/WebAppConfigurer.java
@@ -3,6 +3,7 @@ import com.neusoft.mpserver.common.interceptor.OriginInterceptor;
 import com.neusoft.mpserver.common.interceptor.TokenInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.ArrayList;
@@ -18,6 +19,15 @@ public class WebAppConfigurer implements WebMvcConfigurer {
     public OriginInterceptor getOriginInterceptor(){
         return new OriginInterceptor();
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST")
+                .allowCredentials(false).maxAge(3600);
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //登录拦截
@@ -26,10 +36,10 @@ public class WebAppConfigurer implements WebMvcConfigurer {
         urls.add("/search/**");
         registry.addInterceptor(getTokenInterceptor()).addPathPatterns(urls);
         //跨域拦截
-        List<String> orinigUrls=new ArrayList<String>();
+        /*List<String> orinigUrls=new ArrayList<String>();
         orinigUrls.add("/mark/**");
         orinigUrls.add("/search/**");
         orinigUrls.add("/user/**");
-        registry.addInterceptor(getOriginInterceptor()).addPathPatterns(orinigUrls);
+        registry.addInterceptor(getOriginInterceptor()).addPathPatterns(orinigUrls);*/
     }
 }
